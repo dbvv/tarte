6abccc8b07b5f1afb62577936359d95a7663ccf4
Daniil Borovkov <daniilborowkow@gmail.com>
2020-09-13 10:45:27 +0600

    Feature#284:4

--- template-parts/frontpage-image.php
+++ template-parts/frontpage-image.php
@@ -9,18 +9,15 @@ $product_image_right = carbon_get_theme_option('image_product_left');
 ?>
 
 <div class="frontpage-image">
-  <div class="">
     <div class="row justify-content-between front-page-row" style="align-items: stretch; align-content: stretch;">
-      <div class="col-sm-12 col-md-3 product-image-left" >
+      <div class="col-sm-12 col-md-4 product-image-left" >
       <img src="<?php echo $product_image_left; ?>" alt="" >
       </div>
       <div class="col-sm-12 col-md-4 text-center image-content">
         <?php echo $product_text; ?>
         <br>
-        <a href="<?php echo $product_link; ?>" style="width: 300px;" class="btn btn-primary btn-lg">Купить сейчас</a>
       </div>
-      <div class="product-image-right col-sm-12 col-md-3">
+      <div class="product-image-right col-sm-12 col-md-4">
         <img  src="<?php echo $product_image_right; ?>" alt=""></div>
     </div>
-  </div>
 </div>
